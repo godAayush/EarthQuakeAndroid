add93e1c87c5dbc0ff83ccfa5d4e0b0b8e1ecbdc
root <root@LAPTOP-939ELES7.localdomain>
2020-07-18 04:42:03 +0530

    3.0 customised settings.. just copied

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
@@ -29,7 +30,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -100,7 +104,7 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
             }
         });
 
-        String urlstr = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=5&limit=10";
+        String urlstr = "https://earthquake.usgs.gov/fdsnws/event/1/query";
 
         Bundle bun = new Bundle();
         bun.putString(MY_KEY,urlstr);
@@ -123,7 +127,26 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
     public Loader<List<QuakeData>> onCreateLoader(int id, @Nullable Bundle args) {
         //Log.i("aayush","1st time");
         String urlstr = args.getString(MY_KEY);
-        return new EarthquakeLoader(this,urlstr);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String minMagnitude = sharedPrefs.getString(
+                getString(R.string.settings_min_magnitude_key),
+                getString(R.string.settings_min_magnitude_default));
+
+        String orderBy = sharedPrefs.getString(
+                getString(R.string.settings_order_by_key),
+                getString(R.string.settings_order_by_default)
+        );
+
+        Uri baseUri = Uri.parse(urlstr);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        uriBuilder.appendQueryParameter("format", "geojson");
+        uriBuilder.appendQueryParameter("limit", "10");
+        uriBuilder.appendQueryParameter("minmag", minMagnitude);
+        uriBuilder.appendQueryParameter("orderby", orderBy);
+
+        return new EarthquakeLoader(this, uriBuilder.toString());
     }
 
     @Override
@@ -146,6 +169,30 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         adapter.clear();
         //Log.i("aayush","onloaderreset clear kar diya");
     }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 
 
     /**
